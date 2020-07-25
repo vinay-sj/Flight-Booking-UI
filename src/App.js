83887203715a21be05e8929a27e3d87b2c652d8a
ramtarun <balagam.r@husky.neu.edu>
2020-07-25 19:08:53 -0400

    directory changes fix

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { BrowserRouter ,Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Header from './Header';
 import Homepage from './HomePage';
-import PassengerDetails from './PassengerDetails';
+import PassengerDetails from './components/PassengerDetails';
 
 const App = () => (
   <div>
-      <Header />
-      <div className="container main">
-        <BrowserRouter>
-          <Switch>
-                <Route path="/homepage" component={Homepage} />
-                <Route path="/passengerdetails" component={PassengerDetails} />
-                <Redirect from="/" to="/homepage" />
-            </Switch> 
-        </BrowserRouter>         
-      </div>
+    <Header />
+    <div className="container main">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/homepage" component={Homepage} />
+          <Route path="/passengerdetails" component={PassengerDetails} />
+          <Redirect from="/" to="/homepage" />
+        </Switch>
+      </BrowserRouter>
+    </div>
   </div>
-)
+);
 
 export default App;
