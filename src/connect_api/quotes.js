d1fe4946457fa65773f6af111750d78dcc94d2f0
ramtarun <balagam.r@husky.neu.edu>
2020-07-26 13:14:29 -0400

    format changes

--- src/connect_api/quotes.js
+++ src/connect_api/quotes.js
@@ -1,20 +1,22 @@
-const axios = require("axios");
+const axios = require('axios');
 
 axios({
-    "method":"GET",
-    "url":"https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2019-09-01",
-    "headers":{
-    "content-type":"application/octet-stream",
-    "x-rapidapi-host":"skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-    "x-rapidapi-key":"94187e22cdmshb6f23878dd88f1bp184650jsn983460c6e381",
-    "useQueryString":true
-    },"params":{
-    "inboundpartialdate":"2019-12-01"
-    }
-    })
-    .then((response)=>{
-      console.log(response)
-    })
-    .catch((error)=>{
-      console.log(error)
-    })
+  method: 'GET',
+  url:
+    'https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2019-09-01',
+  headers: {
+    'content-type': 'application/octet-stream',
+    'x-rapidapi-host': 'skyscanner-skyscanner-flight-search-v1.p.rapidapi.com',
+    'x-rapidapi-key': '94187e22cdmshb6f23878dd88f1bp184650jsn983460c6e381',
+    useQueryString: true,
+  },
+  params: {
+    inboundpartialdate: '2019-12-01',
+  },
+})
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
