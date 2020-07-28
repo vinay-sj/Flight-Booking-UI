a7abfb27da6c569e3f65e95a39c5e99181892b42
Dhaval <dhavalmohandas@gmail.com>
2020-07-28 02:16:24 -0400

    Update Get flights request

--- src/connect_api/amadeus.js
+++ src/connect_api/amadeus.js
@@ -1,35 +1,45 @@
 let Amadeus = require('amadeus');
 
+let origin = 'SYD';
+let destination = 'BKK';
+let onwardDate = '2020-08-01';
+let adults = '2';
+
 let amadeus = new Amadeus({
   clientId: 'BASiQ482pDpwH5pvtUEAKlqDM0t4bqRF',
   clientSecret: 'M0zp5ikeGXsmD0Mc'
 });
 
-async function getitenaries(){
+async function getitenaries(origin,destination,onwardDate, adults){
 
     let data = await amadeus.shopping.flightOffersSearch.get({
-        originLocationCode: 'SYD',
-        destinationLocationCode: 'BKK',
-        departureDate: '2020-08-01',
-        adults: '2'
+        originLocationCode: origin,
+        destinationLocationCode: destination,
+        departureDate: onwardDate,
+        adults: adults
     }).then(function(response){
       return(response.data);
     }).catch(function(responseError){
       console.log(responseError.code);
     });
 
-    let itineraries = data.map(({itineraries}) => ({itineraries}));
-    itineraries = itineraries.map(obj => {
-        let val = obj.itineraries[0];
-        return val;
+    let flights = data.map((obj,key) => {
+        let flight = {
+            id: key,
+            price: obj.price.grandTotal,
+            departureDate : obj.itineraries[0].segments[0].departure.at.substring(0, 10),
+            departureTime : obj.itineraries[0].segments[0].departure.at.substring(11, 19),
+            arrivalDate : obj.itineraries[0].segments[0].arrival.at.substring(0, 10),
+            arrivalTime : obj.itineraries[0].segments[0].arrival.at.substring(11, 19),
+            duration : obj.itineraries[0].segments[0].duration.split('T')[1],
+            carrierCode : obj.itineraries[0].segments[0].carrierCode,
+            aircraft: obj.itineraries[0].segments[0].aircraft.code
+        };
 
+        return flight;
     })
-
-    let segments = itineraries.map(obj => {
-        return obj.segments[0];
-    })
-
-    console.log(segments);
+    console.log(flights); // remove this later
+    return(flights);
 }
 
-getitenaries();
+getitenaries(origin,destination,onwardDate,adults);
