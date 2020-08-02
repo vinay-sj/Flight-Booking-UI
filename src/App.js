877c7dca19f26e4984099e71149c15317aab7d9a
Vinay Srampickal Joseph <srampickaljoseph.v@husky.neu.edu>
2020-08-01 23:00:01 -0400

    fixed routing of components

--- src/App.js
+++ src/App.js
@@ -28,25 +28,69 @@ class App extends React.Component {
 		};
 	}
 	render() {
-		if (this.state.confirmBookingFlag && this.state.bookingDetails && this.state.bookingDetails._id) {
-			return (
-				<div>
-					<Header />
-					<div className="container main">
-						<BrowserRouter>
-							<Route path="/bookingConfirmation" render={() => <ConfirmationPage bookingDetails={this.state.bookingDetails} />} />
-							<Redirect to="/bookingConfirmation" />
-						</BrowserRouter>
-					</div>
-				</div>
-			);
-		}
-		if (!this.state.confirmBookingFlag && this.state.bookingDetails && this.state.bookingDetails.onwardFlightDetails) {
-			return (
-				<div>
-					<Header />
-					<div className="container main">
-						<BrowserRouter>
+		// if (this.state.confirmBookingFlag && this.state.bookingDetails && this.state.bookingDetails._id) {
+		// 	return (
+		// 		<div>
+		// 			<Header />
+		// 			<div className="container main">
+		// 				<BrowserRouter>
+		// 					<Route path="/bookingConfirmation" render={() => <ConfirmationPage bookingDetails={this.state.bookingDetails} />} />
+		// 					<Redirect to="/bookingConfirmation" />
+		// 				</BrowserRouter>
+		// 			</div>
+		// 		</div>
+		// 	);
+		// }
+		// if (!this.state.confirmBookingFlag && this.state.bookingDetails && this.state.bookingDetails.onwardFlightDetails) {
+		// 	return (
+		// 		<div>
+		// 			<Header />
+		// 			<div className="container main">
+		// 				<BrowserRouter>
+		// 					<Route
+		// 						path="/passengerdetails"
+		// 						render={() => (
+		// 							<PassengerDetails
+		// 								bookingDetails={this.state.bookingDetails}
+		// 								updateBookingDetails={this.updateBookingDetails}
+		// 								numPassengers={this.state.searchParams.numPassengers}
+		// 							/>
+		// 						)}
+		// 					/>
+		// 					<Redirect to="/passengerdetails" />
+		// 				</BrowserRouter>
+		// 			</div>
+		// 		</div>
+		// 	);
+		// }
+		// if (this.state.searchParams) {
+		// 	return (
+		// 		<div>
+		// 			<Header />
+		// 			<div className="container main">
+		// 				<BrowserRouter>
+		// 					<Route
+		// 						path="/search"
+		// 						render={() => <Search searchParams={this.state.searchParams} updateBookingDetails={this.updateBookingDetails} />}
+		// 					/>
+		// 					<Redirect to="/search" />
+		// 				</BrowserRouter>
+		// 			</div>
+		// 		</div>
+		// 	);
+		// }
+		return (
+			<div>
+				<Header />
+				<div className="container main">
+					<BrowserRouter>
+						<Switch>
+							<Route path="/homepage" render={() => <Homepage flightSearchParams={this.flightSearchParams} />} />
+							{/* <Route path="/passengerdetails" render={() => <PassengerDetails updateBookingDetails={this.updateBookingDetails} />} /> */}
+							<Route
+								path="/search"
+								render={() => <Search searchParams={this.state.searchParams} updateBookingDetails={this.updateBookingDetails} />}
+							/>
 							<Route
 								path="/passengerdetails"
 								render={() => (
@@ -57,36 +101,7 @@ class App extends React.Component {
 									/>
 								)}
 							/>
-							<Redirect to="/passengerdetails" />
-						</BrowserRouter>
-					</div>
-				</div>
-			);
-		}
-		if (this.state.searchParams) {
-			return (
-				<div>
-					<Header />
-					<div className="container main">
-						<BrowserRouter>
-							<Route
-								path="/search"
-								render={() => <Search searchParams={this.state.searchParams} updateBookingDetails={this.updateBookingDetails} />}
-							/>
-							<Redirect to="/search" />
-						</BrowserRouter>
-					</div>
-				</div>
-			);
-		}
-		return (
-			<div>
-				<Header />
-				<div className="container main">
-					<BrowserRouter>
-						<Switch>
-							<Route path="/homepage" render={() => <Homepage flightSearchParams={this.flightSearchParams} />} />
-							{/* <Route path="/passengerdetails" render={() => <PassengerDetails updateBookingDetails={this.updateBookingDetails} />} /> */}
+							{/*<Route path="/bookingConfirmation" render={() => <ConfirmationPage bookingDetails={this.state.bookingDetails} />} />*/}
 							<Route path="/bookings" component={Bookings} />
 							{/*<Route path="/search" render={() => <Search searchParams={this.state.searchParams} />} />*/}
 							<Redirect from="/" to="/homepage" />
