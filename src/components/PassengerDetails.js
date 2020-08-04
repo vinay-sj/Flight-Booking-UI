568ff99f431a859915a01c40a9217ad1a2373157
ramtarun <balagam.r@husky.neu.edu>
2020-08-03 22:13:07 -0400

    stable version of passenger details

--- src/components/PassengerDetails.js
+++ src/components/PassengerDetails.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import { Button, Form, FormGroup, Label, Input, Jumbotron } from 'reactstrap';
+import { Button, Form} from 'reactstrap';
 // import { Select } from 'react-select'
 import ConfirmBookingCall from '../connect_api/confirm_booking';
-// import { LinkContainer } from 'react-router-bootstrap';
-import { useHistory } from 'react-router-dom';
-
-/* Not using LinkContainer as it redirects to confirmation page even before successful POST call. Therefore using 'useHistory' instead */
-
-let confirmBooking, history;
-
-const ConfirmBookingButton = () => {
-	history = useHistory();
-	return <Button onClick={confirmBooking}>Confirm</Button>;
-};
+import { LinkContainer } from 'react-router-bootstrap';
+import PassengerForm from './PassengerForm';
 
 class PassengerDetails extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			userEmail: props.userData ? props.userData.profileObj.email : null,
 			onwardFlightDetails: {
 				journeyDate: props.bookingDetails.onwardFlightDetails.departure.at,
 				flightNo: props.bookingDetails.onwardFlightDetails.carrierCode
@@ -40,143 +30,38 @@ class PassengerDetails extends React.Component {
 			};
 		}
 		this.confirmBooking = this.confirmBooking.bind(this);
-		confirmBooking = this.confirmBooking;
-	}
-
-	componentDidUpdate(prevProps) {
-		if(prevProps.userData !== this.props.userData) {
-			this.setState({userEmail: this.props.userData.profileObj.email});
-		}
+		this.onChange = this.onChange.bind(this);
 	}
 
 	confirmBooking() {
-		if (this.props.userData && this.props.userData.profileObj.email) {
-			ConfirmBookingCall(this.state).then((res) => {
-				this.props.updateBookingDetails(res, true);
-				history.push('/bookingConfirmation');
-			});
-		} else {
-			console.log('Please Login first');
-		}
+		ConfirmBookingCall(this.state).then((res) => {
+			this.props.updateBookingDetails(res, true);
+		});
+	}
+
+	onChange(event,index) {
+		const {name} = event.target;
+		let newState = JSON.parse(JSON.stringify(this.state.passengerDetails));
+		newState[index] = { ...newState[index], [name]: event.target.value };
+		this.setState({
+			passengerDetails: newState,
+		});
 	}
 
 	render() {
 		const { numPassengers } = this.state;
-		const passengerForm = Array.apply(null, { length: numPassengers }).map((e, i) => {
+		const passengerForm = Array.apply(null, { length: numPassengers }).map((e, index) => {
 			return (
-				<Jumbotron key={i}>
-					<FormGroup>
-						<div id={i}>Passenger Details: {i + 1}</div>
-					</FormGroup>
-					<FormGroup>
-						<Label for="name">Name</Label>
-						<Input
-							type="text"
-							name="name"
-							id="name"
-							placeholder="Name"
-							onChange={(event) => {
-								let newState = JSON.parse(JSON.stringify(this.state.passengerDetails));
-								newState[i] = { ...newState[i], name: event.target.value };
-								this.setState({
-									passengerDetails: newState,
-								});
-							}}
-						/>
-					</FormGroup>
-					<FormGroup>
-						<Label for="gender">Gender</Label>
-						<div>
-							<select
-								type="text"
-								id="gender"
-								name="gender"
-								onChange={(event) => {
-									let newState = JSON.parse(JSON.stringify(this.state.passengerDetails));
-									newState[i] = { ...newState[i], gender: event.target.value };
-									this.setState({
-										passengerDetails: newState,
-									});
-								}}
-							>
-								<option value="---">Select a value</option>
-								<option value="M">Male</option>
-								<option value="F">Female</option>
-							</select>
-						</div>
-					</FormGroup>
-					<FormGroup>
-						<Label for="birthDate">Birth Date</Label>
-						<Input
-							type="date"
-							name="birthDate"
-							id="birthDate"
-							placeholder="Birth Date"
-							onChange={(event) => {
-								let newState = JSON.parse(JSON.stringify(this.state.passengerDetails));
-								newState[i] = { ...newState[i], birthDate: event.target.value };
-								this.setState({
-									passengerDetails: newState,
-								});
-							}}
-						/>
-					</FormGroup>
-					<FormGroup>
-						<Label for="email">Email</Label>
-						<Input
-							type="email"
-							name="email"
-							id="email"
-							placeholder="Email"
-							onChange={(event) => {
-								let newState = JSON.parse(JSON.stringify(this.state.passengerDetails));
-								newState[i] = { ...newState[i], emailId: event.target.value };
-								this.setState({
-									passengerDetails: newState,
-								});
-							}}
-						/>
-					</FormGroup>
-					<FormGroup>
-						<Label for="contact">Contact No</Label>
-						<Input
-							type="number"
-							name="contact"
-							id="contact"
-							placeholder="Contact No"
-							onChange={(event) => {
-								let newState = JSON.parse(JSON.stringify(this.state.passengerDetails));
-								newState[i] = { ...newState[i], contactNo: event.target.value };
-								this.setState({
-									passengerDetails: newState,
-								});
-							}}
-						/>
-					</FormGroup>
-					<FormGroup>
-						<Label for="passport">Passport</Label>
-						<Input
-							type="text"
-							name="passport"
-							id="passport"
-							placeholder="Passport No"
-							onChange={(event) => {
-								let newState = JSON.parse(JSON.stringify(this.state.passengerDetails));
-								newState[i] = { ...newState[i], passPortNo: event.target.value };
-								this.setState({
-									passengerDetails: newState,
-								});
-							}}
-						/>
-					</FormGroup>
-				</Jumbotron>
+				<PassengerForm key={index} onChange={this.onChange} index={index}/>
 			);
 		});
 
 		return (
 			<Form>
 				{passengerForm}
-				<ConfirmBookingButton />
+				<LinkContainer to={'/bookingConfirmation'}>
+					<Button onClick={this.confirmBooking}>Confirm</Button>
+				</LinkContainer>
 			</Form>
 		);
 	}
