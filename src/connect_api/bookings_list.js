32f34ab8d7743690f719deb4415fb1ee4fa18165
ramtarun <balagam.r@husky.neu.edu>
2020-08-05 18:36:29 -0400

    eslint fixes

--- src/connect_api/bookings_list.js
+++ src/connect_api/bookings_list.js
@@ -1,117 +1,117 @@
 const axios = require('axios');
 
 export async function getRoundTripBookings()  {
-    
-    let roundTripBookings;
-    try{
-        roundTripBookings = await axios({
-            method: 'GET',
-            url: 'http://localhost:5000/api/bookings/roundTripBookings',
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-            }
-        }).then((response) => {
+
+	let roundTripBookings;
+	try{
+		roundTripBookings = await axios({
+			method: 'GET',
+			url: 'http://localhost:5000/api/bookings/roundTripBookings',
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+			}
+		}).then((response) => {
 			return response;
 		})
-		.then(({ data }) => {
-			return(data);
-        });
+			.then(({ data }) => {
+				return(data);
+			});
 
-        roundTripBookings = roundTripBookings.filter((booking) => booking.isRoundTrip);
+		roundTripBookings = roundTripBookings.filter((booking) => booking.isRoundTrip);
 
-        roundTripBookings = roundTripBookings.map((obj) => {
-            obj.names = obj.passengerDetails.map(({name}) => name);
-            obj.onwardFlightNo = obj.onwardFlightDetails.flightNo;
-            obj.onwardAirlineName = obj.onwardFlightDetails.airlineName;
-            obj.onwardJourneyDate = obj.onwardFlightDetails.journeyDate;
-            obj.returnFlightNo = obj.returnFlightDetails.flightNo;
-            obj.returnAirlineName = obj.returnFlightDetails.airlineName;
-            obj.returnJourneyDate = obj.returnFlightDetails.journeyDate;
-            delete obj.onwardFlightDetails;
-            delete obj.returnFlightDetails;
-            delete obj.passengerDetails;
-            delete obj.__v;
-            delete obj.bookingDate;
-            return obj;
-        });
+		roundTripBookings = roundTripBookings.map((obj) => {
+			obj.names = obj.passengerDetails.map(({name}) => name);
+			obj.onwardFlightNo = obj.onwardFlightDetails.flightNo;
+			obj.onwardAirlineName = obj.onwardFlightDetails.airlineName;
+			obj.onwardJourneyDate = obj.onwardFlightDetails.journeyDate;
+			obj.returnFlightNo = obj.returnFlightDetails.flightNo;
+			obj.returnAirlineName = obj.returnFlightDetails.airlineName;
+			obj.returnJourneyDate = obj.returnFlightDetails.journeyDate;
+			delete obj.onwardFlightDetails;
+			delete obj.returnFlightDetails;
+			delete obj.passengerDetails;
+			delete obj.__v;
+			delete obj.bookingDate;
+			return obj;
+		});
 
-        roundTripBookings = roundTripBookings.sort((a,b) => (a.onwardJourneyDate > b.onwardJourneyDate ) ? 1 : -1 );
+		roundTripBookings = roundTripBookings.sort((a,b) => (a.onwardJourneyDate > b.onwardJourneyDate ) ? 1 : -1 );
 
-    }catch(err){
-        console.log(err);
-    }
-    //console.log(roundTripBookings);
-    return roundTripBookings;
-    
+	}catch(err){
+		console.log(err);
+	}
+	//console.log(roundTripBookings);
+	return roundTripBookings;
+
 }
 
 export async function getOneWayBookings()  {
-    
-    let oneWayBookings;
-    try{
-        oneWayBookings = await axios({
-            method: 'GET',
-            url: 'http://localhost:5000/api/bookings/oneWayBookings',
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-            }
-        }).then((response) => {
+
+	let oneWayBookings;
+	try{
+		oneWayBookings = await axios({
+			method: 'GET',
+			url: 'http://localhost:5000/api/bookings/oneWayBookings',
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+			}
+		}).then((response) => {
 			return response;
 		})
-		.then(({ data }) => {
-			return(data);
-        });
+			.then(({ data }) => {
+				return(data);
+			});
 
-        oneWayBookings = oneWayBookings.filter((booking) => !booking.isRoundTrip);
-        
-        oneWayBookings = oneWayBookings.map((obj) => {
-            obj.names = obj.passengerDetails.map(({name}) => name);
-            obj.onwardFlightNo = obj.onwardFlightDetails.flightNo;
-            obj.onwardAirlineName = obj.onwardFlightDetails.airlineName;
-            obj.onwardJourneyDate = obj.onwardFlightDetails.journeyDate;
-            if(obj.returnFlightDetails){
-                delete obj.returnFlightDetails;
-            }
-            delete obj.onwardFlightDetails;
-            delete obj.passengerDetails;
-            delete obj.__v;
-            delete obj.bookingDate;
-            return obj;
-        });
+		oneWayBookings = oneWayBookings.filter((booking) => !booking.isRoundTrip);
 
-        oneWayBookings = oneWayBookings.sort((a,b) => (a.onwardJourneyDate > b.onwardJourneyDate ) ? 1 : -1 );
+		oneWayBookings = oneWayBookings.map((obj) => {
+			obj.names = obj.passengerDetails.map(({name}) => name);
+			obj.onwardFlightNo = obj.onwardFlightDetails.flightNo;
+			obj.onwardAirlineName = obj.onwardFlightDetails.airlineName;
+			obj.onwardJourneyDate = obj.onwardFlightDetails.journeyDate;
+			if(obj.returnFlightDetails){
+				delete obj.returnFlightDetails;
+			}
+			delete obj.onwardFlightDetails;
+			delete obj.passengerDetails;
+			delete obj.__v;
+			delete obj.bookingDate;
+			return obj;
+		});
+
+		oneWayBookings = oneWayBookings.sort((a,b) => (a.onwardJourneyDate > b.onwardJourneyDate ) ? 1 : -1 );
 
 
-    }catch(err){
-        console.log(err);
-    }
-    //console.log(oneWayBookings);
-    return oneWayBookings;
-    
+	}catch(err){
+		console.log(err);
+	}
+	//console.log(oneWayBookings);
+	return oneWayBookings;
+
 }
 
 export async function deleteBooking(isRoundTrip, id){
 
-    let success;
-    let url;
-    if(isRoundTrip){
-        url = 'http://localhost:5000/api/bookings/deleteRoundTrip/'.concat('',id) ;
-    }else{
-        url = 'http://localhost:5000/api/bookings/deleteOneWayTrip/'.concat('',id) ;
-    }
-   
-    try{
-        success = await axios.delete(url).then((response) => {
+	let success;
+	let url;
+	if(isRoundTrip){
+		url = 'http://localhost:5000/api/bookings/deleteRoundTrip/'.concat('',id) ;
+	}else{
+		url = 'http://localhost:5000/api/bookings/deleteOneWayTrip/'.concat('',id) ;
+	}
+
+	try{
+		success = await axios.delete(url).then((response) => {
 			return response;
-		})
-    }
-    catch(err){
-        console.log(err);
-    }
-    //console.log(success);
-    return success;
+		});
+	}
+	catch(err){
+		console.log(err);
+	}
+	//console.log(success);
+	return success;
 
 }
 
--- src/connect_api/passengers.js
+++ src/connect_api/passengers.js
@@ -1,34 +1,34 @@
 const axios = require('axios');
 
 export async function getPassengers()  {
-    
-    let passengers;
-    try{
-        passengers = await axios({
-            method: 'GET',
-            url: 'http://localhost:5000/api/passengers/getPassenger',
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-            }
-        }).then((response) => {
+
+	let passengers;
+	try{
+		passengers = await axios({
+			method: 'GET',
+			url: 'http://localhost:5000/api/passengers/getPassenger',
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+			}
+		}).then((response) => {
 			return response;
 		})
-		.then(({ data }) => {
-			return(data);
-        });
+			.then(({ data }) => {
+				return(data);
+			});
 
 
-    }catch(err){
-        console.log(err);
-    }
-    //console.log(passengers);
-    return passengers;
-    
+	}catch(err){
+		console.log(err);
+	}
+	//console.log(passengers);
+	return passengers;
+
 }
 
 export async function addPassenger(reqBody) {
-    return axios({
+	return axios({
 		method: 'POST',
 		url: 'http://localhost:5000/api/passengers/addPassenger',
 		headers: {
@@ -42,16 +42,16 @@ export async function addPassenger(reqBody) {
 
 export async function deletePassenger(id) {
 
-    let success;
-    let url = 'http://localhost:5000/api/passengers/deletePassenger/'.concat('',id);
-    try{
-        success = await axios.delete(url).then((response) => {
+	let success;
+	let url = 'http://localhost:5000/api/passengers/deletePassenger/'.concat('',id);
+	try{
+		success = await axios.delete(url).then((response) => {
 			return response;
-		})
-    }
-    catch(err){
-        console.log(err);
-    }
-    console.log(success);
-    return success;
+		});
+	}
+	catch(err){
+		console.log(err);
+	}
+	console.log(success);
+	return success;
 }
