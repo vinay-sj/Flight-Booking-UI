1fca1cc1248524feaf5bedebae977f8386d90842
ramtarun <balagam.r@husky.neu.edu>
2020-08-07 00:02:36 -0400

    Handled Backend Authentication using JWT Tokens

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,54 +1,86 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Navbar, NavbarBrand, NavItem, Nav, NavLink } from 'reactstrap';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
 let userName;
 
-const RenderLoginButtons = ({updateUserDetails}) => {
 
-	const loginHandler = (loginResponse) => {
+class Example extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isUserLoggedIn: false
+		};
+
+		this.loginHandler = this.loginHandler.bind(this);
+		this.logoutHandler = this.logoutHandler.bind(this);
+	}
+
+	async loginHandler(loginResponse) {
 		userName = loginResponse && loginResponse.profileObj ? loginResponse.profileObj.name : null;
-		updateUserDetails(loginResponse);
-		setLoginState(!isUserLoggedIn);
-	};
+		if (loginResponse.tokenId) {
+			const serverSignInResponse = await fetch('http://localhost:5000/auth/signin', {
+				method: 'POST',
+				credentials: 'include',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ google_tokenId: loginResponse.tokenId }),
+			});
+			const body = await serverSignInResponse.text();
+			const result = JSON.parse(body);
+			const { signedIn, givenName } = result;
+			console.log({ signedIn, givenName });
+		}
+		this.props.updateUserDetails(loginResponse);
+		this.setState({isUserLoggedIn: !this.state.isUserLoggedIn});
+	}
 
-	const [isUserLoggedIn, setLoginState] = useState(false);
+	async logoutHandler(logoutresponse) {
+		const serverSignOutResponse = await fetch('http://localhost:5000/auth/signout', {
+			method: 'POST',
+			credentials: 'include',
+		});
+		const body = await serverSignOutResponse.text();
+		const result = JSON.parse(body);
+		console.log(result);
+		this.props.updateUserDetails(logoutresponse);
+		this.setState({isUserLoggedIn: !this.state.isUserLoggedIn});
+	}
 
-	return !isUserLoggedIn ? (
-		<GoogleLogin
-			clientId="660147973995-blt1grgbfeebedrr543u53s7okhgtlso.apps.googleusercontent.com"
-			buttonText="Login"
-			onSuccess={(res) => loginHandler(res)}
-			onFailure={(err) => console.log(err)}
-			cookiePolicy={'single_host_origin'}
-			isSignedIn={true}
-		/>
-	) : (
-		<GoogleLogout
-			clientId="660147973995-blt1grgbfeebedrr543u53s7okhgtlso.apps.googleusercontent.com"
-			buttonText={`Logout ${userName}`}
-			onLogoutSuccess={(res) => loginHandler(res)}
-		/>
-	);
-};
-
-const Example = ({updateUserDetails}) => {
-	return (
-		<div>
-			<Navbar expand="md">
-				<NavbarBrand className="mr-auto" href="/">Home</NavbarBrand>
-				<Nav className="mr-auto" navbar>
-					<NavItem>
-						<NavLink href="/bookings">Bookings</NavLink>
-					</NavItem>
-					<NavItem>
-						<NavLink href="/passengerlist">Saved Passengers</NavLink>
-					</NavItem>
-				</Nav>
-				<RenderLoginButtons updateUserDetails={updateUserDetails}/>
-			</Navbar>
-		</div>
-	);
-};
+	render() {
+		return (
+			<div>
+				<Navbar expand="md">
+					<NavbarBrand className="mr-auto" href="/">Home</NavbarBrand>
+					<Nav className="mr-auto" navbar>
+						<NavItem>
+							<NavLink href="/bookings">My Bookings</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink href="/passengerlist">My Passengers</NavLink>
+						</NavItem>
+					</Nav>
+					{!this.state.isUserLoggedIn ? (
+						<GoogleLogin
+							clientId="660147973995-blt1grgbfeebedrr543u53s7okhgtlso.apps.googleusercontent.com"
+							buttonText="Login"
+							onSuccess={(res) => this.loginHandler(res)}
+							onFailure={(err) => console.log(err)}
+							cookiePolicy={'single_host_origin'}
+							isSignedIn={true}
+						/>
+					) : (
+						<GoogleLogout
+							clientId="660147973995-blt1grgbfeebedrr543u53s7okhgtlso.apps.googleusercontent.com"
+							buttonText={`Logout ${userName}`}
+							onLogoutSuccess={(res) => this.logoutHandler(res)}
+						/>
+					)}
+				</Navbar>
+			</div>
+		);
+	}
+}
 
 export default Example;
--- src/connect_api/confirm_booking.js
+++ src/connect_api/confirm_booking.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 const ConfirmBookingCall = (reqBody) => {
 	return axios({
 		method: 'POST',
-		//url: 'http://localhost:5000/api/bookings/confirmBooking',
-		url: 'https://group-project-avengers-api.herokuapp.com/api/bookings/confirmBooking',
+		url: 'http://localhost:5000/api/bookings/confirmBooking',
+		// url: 'https://group-project-avengers-api.herokuapp.com/api/bookings/confirmBooking',
+		withCredentials: true,
 		headers: {
-			'Access-Control-Allow-Origin': '*',
-			'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+			'Content-Type': 'application/json',
 		},
 		data: reqBody,
 	});
