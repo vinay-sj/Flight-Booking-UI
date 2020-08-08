e76d09a76669f078c37a0e619d98f03fad0d801f
Vinay Srampickal Joseph <srampickaljoseph.v@husky.neu.edu>
2020-08-08 14:31:23 -0400

    edit passenger details ui

--- src/components/SavedPassengerList.js
+++ src/components/SavedPassengerList.js
@@ -2,17 +2,20 @@ import React from 'react';
 import { Button, ButtonGroup, Modal, ModalFooter, ModalBody } from 'reactstrap';
 import { Well, Glyphicon } from 'react-bootstrap';
 import PassengerFormTemplate from './PassengerFormTemplate';
-import { getPassengers, addPassenger, deletePassenger } from '../connect_api/passengers';
+import { getPassengers, addPassenger, deletePassenger, editPassenger } from '../connect_api/passengers';
 import PassengerListTable from './PassengerListTable';
 
 const ActionButtons = (props) => {
-	const { deletePassengers, index} = props;
+	const { deletePassengers, index, editPassengers } = props;
 	const onDelete = () => {
 		deletePassengers(index);
 	};
+	const onEdit = () => {
+		editPassengers(index);
+	}
 	return(
 		<ButtonGroup className="btn-group-sm">
-			<Button className='btn btn-light buttonTheme' ><Glyphicon glyph="edit"/>Edit</Button>
+			<Button className='btn btn-light buttonTheme' onClick={onEdit}><Glyphicon glyph="edit"/>Edit</Button>
 			<Button className='btn btn-light buttonTheme' onClick={onDelete}> <Glyphicon glyph="trash"/>Delete</Button>
 		</ButtonGroup>
 	);
@@ -23,23 +26,41 @@ class Passengers extends React.Component {
 		super(props);
 		this.state={
 			modal:false,
-			passengerDetails:[],
+			editModal: false,
 			passengerList: [],
+			editIndex: null,
 		};
 
 		this.savePassenger = this.savePassenger.bind(this);
 		this.toggle = this.toggle.bind(this);
 		this.onChange = this.onChange.bind(this);
+		this.onUpdateChange = this.onUpdateChange.bind(this);
 		this.onDatePickerChange = this.onDatePickerChange.bind(this);
+		this.onUpdateDatePickerChange = this.onUpdateDatePickerChange.bind(this);
 		this.deletePassengers = this.deletePassengers.bind(this);
 		this.loadData = this.loadData.bind(this);
+		this.editPassengers = this.editPassengers.bind(this);
+		this.updateEditPassenger = this.updateEditPassenger.bind(this);
+		this.editToggle = this.editToggle.bind(this);
 	}
 
 	async savePassenger() {
 		const { passengerDetails } = this.state;
-		await addPassenger(passengerDetails[0]);
+		await addPassenger(passengerDetails);
+		this.setState({passengerDetails:{}});
 		await this.loadData();
 		this.toggle();
+	}
+
+	async updateEditPassenger() {
+		const { passengerList, editIndex } = this.state;
+		console.log('edit')
+		console.log(passengerList[editIndex])
+		await editPassenger(passengerList[editIndex]._id, passengerList[editIndex]);
+		await this.loadData();
+		this.editToggle();
+		this.setState({passengerDetails:{}});
+		this.setState({ editIndex: null });
 	}
 
 	toggle() {
@@ -47,20 +68,38 @@ class Passengers extends React.Component {
 		this.setState({ modal:!modal });
 	}
 
-	onChange(event,index) {
-		const { name } = event.target;
-		let newState = JSON.parse(JSON.stringify(this.state.passengerDetails));
-		newState[index] = { ...newState[index], [name]: event.target.value };
+	editToggle() {
+		const { editModal } = this.state;
+		this.setState({ editModal: !editModal });
+	}
+
+	onUpdateChange(event, index) {
+		const { name, value } = event.target;
+		let newState = JSON.parse(JSON.stringify(this.state.passengerList));
+		newState[index] = { ...newState[index], [name]: value };
 		this.setState({
-			passengerDetails: newState,
+			passengerList: newState,
 		});
 	}
 
-	onDatePickerChange(date, name, index) {
-		let newState = JSON.parse(JSON.stringify(this.state.passengerDetails));
+	onChange(event) {
+		const { name, value } = event.target;
+		this.setState({
+				passengerDetails: {...this.state.passengerDetails, [name]:value}
+			});
+	}
+
+	onUpdateDatePickerChange(date, name, index) {
+		let newState = JSON.parse(JSON.stringify(this.state.passengerList));
 		newState[index] = { ...newState[index], [name]: date };
 		this.setState({
-			passengerDetails: newState,
+			passengerList: newState,
+		});
+	}
+
+	onDatePickerChange(date, name) {
+		this.setState({
+			passengerDetails: {...this.state.passengerDetails, [name]:date}
 		});
 	}
 
@@ -85,8 +124,15 @@ class Passengers extends React.Component {
 		await this.loadData();
 	}
 
+	editPassengers(index) {
+		this.setState({ editIndex: index });
+		this.editToggle();
+	}
+
+
 	render() {
-		const { modal, passengerList } = this.state;
+		const { modal, editModal, passengerList, editIndex } = this.state;
+
 		return (
 			<>
 				<Well bsSize="small">
@@ -104,7 +150,16 @@ class Passengers extends React.Component {
 						</ModalFooter>
 					</Modal>
 				</Well>
-				<PassengerListTable passengers={passengerList} actionButtons={(index)=>{return (<ActionButtons index={index} deletePassengers={this.deletePassengers}/>);}} />
+				<PassengerListTable passengers={passengerList} actionButtons={(index)=>{return (<ActionButtons index={index} deletePassengers={this.deletePassengers} editPassengers={this.editPassengers} />);}} />
+				<Modal isOpen={editModal} toggle={this.editToggle}>
+					<ModalBody>
+						<PassengerFormTemplate onChange={this.onUpdateChange} index={editIndex} onDatePickerChange={this.onUpdateDatePickerChange} addPassenger={null} passengerValue={passengerList[editIndex]} />
+					</ModalBody>
+					<ModalFooter>
+						<Button className='btn btn-light buttonTheme' color="primary" onClick={this.updateEditPassenger}>Update</Button>{' '}
+						<Button className='btn btn-light buttonTheme' color="secondary" onClick={this.editToggle}>Cancel</Button>
+					</ModalFooter>
+				</Modal>
 			</>
 		);
 	}
