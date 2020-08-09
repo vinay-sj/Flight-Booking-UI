75d9fcd78bdf75404b5fe887365a4aebcea2eb5f
Vinay Srampickal Joseph <srampickaljoseph.v@husky.neu.edu>
2020-08-09 13:07:41 -0400

    about page with json data

--- src/components/About.js
+++ src/components/About.js
@@ -3,53 +3,47 @@ import {
   Row, Col, Card, CardImg, CardBody, CardTitle, NavLink
 } from 'reactstrap';
 
+/**
+ * Class for the About page. It explains why we made this project and the details of the team members.
+ */
 class About extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      teamMembers:[],
+    }
+  }
+
+  componentDidMount() {
+    const json = require('../mock_json/about.json');
+    const teamMembers = JSON.parse(JSON.stringify(json));
+    this.setState({ teamMembers: teamMembers });
+    console.log(teamMembers[0].avatar)
+  }
+
   render() {
+    const { teamMembers } = this.state;
+    const teamInfo = (teamMembers.map((member, index)=>{
+      return (
+        <Col md={3} key={index}>
+          <Card>
+            <CardImg top width="100%" src={member.avatar} alt={member.name}></CardImg>
+            <CardBody className='text-center'>
+              <CardTitle>{member.name}</CardTitle>
+              <NavLink href={member.linkedin} target='_blank'>LinkedIn</NavLink>
+            </CardBody>
+          </Card>
+        </Col>
+      );
+    }));
+
     return (
       <>
         <h3 className='text-center'>About Us</h3>
         <p>We are a team of 4 ....</p>
-
         <h4 className='text-center'>****Meet the Team****</h4>
         <Row>
-          <Col md={3}>
-            <Card>
-              <CardImg top width="100%" src={require("../img/dhaval.jpg")} alt="Dhaval Mohandas"></CardImg>
-              <CardBody className='text-center'>
-                <CardTitle>Dhaval</CardTitle>
-                <NavLink href="https://www.linkedin.com/in/dhaval-mohandas/" target='_blank'>LinkedIn</NavLink>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col md={3}>
-            <Card>
-              <CardImg top width="100%" src={require("../img/priyank.jpg")} alt="Priyank"></CardImg>
-              <CardBody className='text-center'>
-                <CardTitle>Priyank</CardTitle>
-                <NavLink href="https://www.linkedin.com/in/priyank-s-2a94118a/" target='_blank'>LinkedIn</NavLink>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col md={3}>
-            <Card>
-              <CardImg top width="100%" src={require("../img/ram.jpg")} alt="Ram"></CardImg>
-              <CardBody className='text-center'>
-                <CardTitle>Ram</CardTitle>
-                <NavLink href="https://www.linkedin.com/in/ram-tarun-balagam-a0986a113/" target='_blank'>LinkedIn</NavLink>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col md={3}>
-            <Card>
-              <CardImg top width="100%" src={require("../img/vinay.jpg")} alt="Vinay Srampickal Joseph"></CardImg>
-              <CardBody className='text-center'>
-                <CardTitle>
-                  <div>Vinay</div></CardTitle>
-                <NavLink href="https://www.linkedin.com/in/vinaysj/" target='_blank'>LinkedIn</NavLink>
-              </CardBody>
-            </Card>
-          </Col>
-
+          {teamInfo}
         </Row>
       </>
     );
