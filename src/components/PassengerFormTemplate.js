cf782ee2c9274a104af015bc318769480aef5e04
ramtarun <balagam.r@husky.neu.edu>
2020-08-10 23:39:34 -0400

    minor bug fix

--- src/components/PassengerFormTemplate.js
+++ src/components/PassengerFormTemplate.js
@@ -1,4 +1,4 @@
-import { FormGroup, Input, Jumbotron, Label, Row, FormFeedback, Col, Form } from 'reactstrap';
+import { FormGroup, Input, Jumbotron, Label, Row, FormFeedback, Col } from 'reactstrap';
 import React from 'react';
 import DatePicker from 'react-datepicker';
 
@@ -45,135 +45,132 @@ class PassengerFormTemplate extends React.Component {
 					<div className='col-form-label form-inline'>{addPassenger}</div>
 				</div>
 				<Jumbotron key={index}>
-					<Form>
-						<FormGroup row>
-							<Col id={index}>Passenger Details: {passNo}</Col>
-						</FormGroup>
-						<FormGroup row>
-							<Col sm={12}>
-								<Label for="name">Name</Label>
-							</Col>
-							<Col sm={12}>
-								<Input
-									type="text"
-									name="name"
-									id="name"
-									value={passengerValue.name||this.state.name}
-									placeholder="Name"
-									onChange={(event) => {
-										this.updateState(event);
-										onChange(event, i);
-									}}
-								/>
-							</Col>
-						</FormGroup>
-						<Row form>
-							<Col className='col-6 col-sm-6'>
-								<FormGroup >
-									<Label for="gender">Gender</Label>
-									<div>
-										<Input
-											type="select"
-											id="gender"
-											value={passengerValue.gender||this.state.gender}
-											name="gender"
-											onChange={(event) => {
-												this.updateState(event);
-												onChange(event, i);
-											}}
-										>
-											<option>Select Gender</option>
-											<option value={'M'}>Male</option>
-											<option value={'F'}>Female</option>
-										</Input>
-									</div>
-								</FormGroup>
+					<FormGroup row>
+						<Col id={index}>Passenger Details: {passNo}</Col>
+					</FormGroup>
+					<FormGroup row>
+						<Col sm={12}>
+							<Label for="name">Name</Label>
+						</Col>
+						<Col sm={12}>
+							<Input
+								type="text"
+								name="name"
+								id="name"
+								value={passengerValue.name||this.state.name}
+								placeholder="Name"
+								onChange={(event) => {
+									this.updateState(event);
+									onChange(event, i);
+								}}
+							/>
+						</Col>
+					</FormGroup>
+					<Row form>
+						<Col className='col-6 col-sm-6'>
+							<FormGroup >
+								<Label for="gender">Gender</Label>
+								<div>
+									<Input
+										type="select"
+										id="gender"
+										value={passengerValue.gender||this.state.gender}
+										name="gender"
+										onChange={(event) => {
+											this.updateState(event);
+											onChange(event, i);
+										}}
+									>
+										<option>Select Gender</option>
+										<option value={'M'}>Male</option>
+										<option value={'F'}>Female</option>
+									</Input>
+								</div>
+							</FormGroup>
 
-							</Col>
-							<Col className='col-6 col-sm-6'>
-								<FormGroup>
-									<Label for="birthDate">Birth Date</Label>
-									<div sm={12}>
-										<DatePicker
-											id='birthDate'
-											maxDate={new Date()}
-											selected={(passengerValue.birthDate) ? new Date(passengerValue.birthDate) : birthDate}
-											onChange={(date) => {
-												this.updateBirthDate(date);
-												onDatePickerChange(date, 'birthDate', i);
-											}}
-											className='custom-date-picker-css col-sm-12'
-										/>
-									</div>
-								</FormGroup>
-							</Col>
-						</Row>
-						<Row form>
-							<Col md={6}>
-								<FormGroup>
-									<Label for="email">Email</Label>
-									<div>
-										<Input
-											invalid={ this.props.validate.emailState }
-											type="email"
-											name="emailId"
-											defaultValue={passengerValue.emailId||this.state.emailId}
-											id="email"
-											placeholder="Email"
-											onBlur={(event) => {
-												this.updateState(event);
-												this.props.validateEmail(event, i);
-											}}
-										/>
-										<FormFeedback>Please enter email in the proper format: wanda@maximoff.com</FormFeedback>
-									</div>
+						</Col>
+						<Col className='col-6 col-sm-6'>
+							<FormGroup>
+								<Label for="birthDate">Birth Date</Label>
+								<div sm={12}>
+									<DatePicker
+										id='birthDate'
+										maxDate={new Date()}
+										selected={(passengerValue.birthDate) ? new Date(passengerValue.birthDate) : birthDate}
+										onChange={(date) => {
+											this.updateBirthDate(date);
+											onDatePickerChange(date, 'birthDate', i);
+										}}
+										className='custom-date-picker-css col-sm-12'
+									/>
+								</div>
+							</FormGroup>
+						</Col>
+					</Row>
+					<Row form>
+						<Col md={6}>
+							<FormGroup>
+								<Label for="email">Email</Label>
+								<div>
+									<Input
+										invalid={ this.props.validate.emailState }
+										type="email"
+										name="emailId"
+										defaultValue={passengerValue.emailId||this.state.emailId}
+										id="email"
+										placeholder="Email"
+										onBlur={(event) => {
+											this.updateState(event);
+											this.props.validateEmail(event, i);
+										}}
+									/>
+									<FormFeedback>Please enter email in the proper format: wanda@maximoff.com</FormFeedback>
+								</div>
 
-								</FormGroup>
-							</Col>
-							<Col md={6}>
-								<FormGroup>
-									<Label for="contact">Contact No</Label>
-									<div>
-										<Input
+							</FormGroup>
+						</Col>
+						<Col md={6}>
+							<FormGroup>
+								<Label for="contact">Contact No</Label>
+								<div>
+									<Input
 										//invalid={ this.state.validate.phoneState }
-											type="text"
-											name="contactNo"
-											defaultValue={passengerValue.contactNo||this.state.contactNo}
-											id="contact"
-											placeholder="Contact No"
-											onChange={(event) => {
-												this.updateState(event);
-												onChange(event, i);
-											}}
-										/>
-										<FormFeedback>Please enter phone number in the proper format</FormFeedback>
-									</div>
-								</FormGroup>
-							</Col>
-						</Row>
+										type="text"
+										name="contactNo"
+										defaultValue={passengerValue.contactNo||this.state.contactNo}
+										id="contact"
+										placeholder="Contact No"
+										onChange={(event) => {
+											this.updateState(event);
+											onChange(event, i);
+										}}
+									/>
+									<FormFeedback>Please enter phone number in the proper format</FormFeedback>
+								</div>
+							</FormGroup>
+						</Col>
+					</Row>
 
-
-						<FormGroup row>
-							<Col sm={12}>
-								<Label for="passport">Passport Number</Label>
-							</Col>
-							<Col sm={12}>
-								<Input
-									invalid={ this.props.validate.passState }
-									type="text"
-									name="passPortNo"
-									id="passport"
-									defaultValue={passengerValue.passPortNo||this.state.passPortNo}
-									placeholder="Passport No"
-									onBlur={(event) => {
-										this.updateState(event);
-										this.props.validatePassport(event, i);
-									}}
-								/>
-								<FormFeedback>Please enter passport number in the proper format: AB1234567</FormFeedback>
-							</Col>
-						</FormGroup>
-					</Form>
+					<FormGroup row>
+						<Col sm={12}>
+							<Label for="passport">Passport Number</Label>
+						</Col>
+						<Col sm={12}>
+							<Input
+								invalid={ this.props.validate.passState }
+								type="text"
+								name="passPortNo"
+								id="passport"
+								defaultValue={passengerValue.passPortNo||this.state.passPortNo}
+								placeholder="Passport No"
+								onBlur={(event) => {
+									this.updateState(event);
+									this.props.validatePassport(event, i);
+								}}
+							/>
+							<FormFeedback>Please enter passport number in the proper format: AB1234567</FormFeedback>
+						</Col>
+					</FormGroup>
 				</Jumbotron>
 			</>
 		);
