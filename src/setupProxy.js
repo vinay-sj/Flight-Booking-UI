08ffbfad21d917020f22b6647cd7239109cf873b
ramtarun <balagam.r@husky.neu.edu>
2020-08-12 10:25:29 -0400

    Changes for Heroku

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,18 +1,18 @@
-const proxy = require("http-proxy-middleware");
+const proxy = require('http-proxy-middleware');
 
 module.exports = app => {
-  app.use(
-    "/auth",
-    proxy({
-      target: process.env.REACT_APP_PROXY_API_ENDPOINT,
-      changeOrigin: true
-    })
-  );
-  app.use(
-    "/api",
-    proxy({
-      target: process.env.REACT_APP_PROXY_API_ENDPOINT,
-      changeOrigin: true
-    })
-  );
+	app.use(
+		'/auth',
+		proxy({
+			target: process.env.REACT_APP_PROXY_API_ENDPOINT,
+			changeOrigin: true
+		})
+	);
+	app.use(
+		'/api',
+		proxy({
+			target: process.env.REACT_APP_PROXY_API_ENDPOINT,
+			changeOrigin: true
+		})
+	);
 };
